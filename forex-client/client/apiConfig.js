90c1d66215ad6e7295678b37ae0829b8c8bba262
MBUYT0n <shusrith03@gmail.com>
2025-04-15 12:05:32 +0530

    goodness

--- forex-client/client/apiConfig.js
+++ forex-client/client/apiConfig.js
@@ -1,5 +1,6 @@
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
-const DUMMY_API_URL = process.env.REACT_APP_DUMMY_API_URL || 'http://localhost:3001';
-const MASTER_URL = process.env.REACT_APP_MASTER_URL || 'http://localhost:5000';
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:4000";
+const DUMMY_API_URL =
+	process.env.REACT_APP_DUMMY_API_URL || "http://localhost:3001";
+const MASTER_URL = process.env.REACT_APP_MASTER_URL || "http://localhost:5000";
 
 export { BASE_URL, DUMMY_API_URL, MASTER_URL };
