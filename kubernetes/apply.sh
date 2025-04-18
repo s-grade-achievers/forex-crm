7e9773e3be5c2dcdc1f2259b4a42f72b6d105aeb
MBUYT0n <shusrith03@gmail.com>
2025-04-18 07:59:16 +0530

    .

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -1,24 +1,22 @@
 kubectl apply -f namespace.yaml
-kubectl apply -f forex-admin-secret.yaml
-kubectl apply -f mssql-secret.yaml
 kubectl apply -f jwt-secret.yaml
 kubectl apply -f pvc.yaml
 kubectl apply -f postgres-secret.yaml
-kubectl create configmap mssql-init-scripts --from-file=../forex-admin/db/ -n forex-crm
+kubectl create configmap postgres1-initdb-script --from-file=../forex-admin/db/init.sql -n forex-crm
 kubectl create configmap postgres-initdb-script --from-file=../forex-client/initdb.sql -n forex-crm
-kubectl apply -f mssql-deployment.yaml
-kubectl apply -f mssql-service.yaml
+kubectl apply -f postgres1-deployment.yaml
+kubectl apply -f postgres1-service.yaml
 kubectl apply -f forex-admin-deployment.yaml
 kubectl apply -f forex-admin-service.yaml
-kubectl apply -f postgres-deployment.yaml
-kubectl apply -f postgres-service.yaml
-kubectl apply -f backend-deployment.yaml
-kubectl apply -f backend-service.yaml
-kubectl apply -f frontend-deployment.yaml
-kubectl apply -f frontend-service.yaml
-kubectl apply -f web-deployment.yaml
-kubectl apply -f web-service.yaml
-kubectl apply -f loyalty-deployment.yaml
-kubectl apply -f loyalty-service.yaml
+# kubectl apply -f postgres-deployment.yaml
+# kubectl apply -f postgres-service.yaml
+# kubectl apply -f backend-deployment.yaml
+# kubectl apply -f backend-service.yaml
+# kubectl apply -f frontend-deployment.yaml
+# kubectl apply -f frontend-service.yaml
+# kubectl apply -f web-deployment.yaml
+# kubectl apply -f web-service.yaml
+# kubectl apply -f loyalty-deployment.yaml
+# kubectl apply -f loyalty-service.yaml
 
 # kubectl delete namespace forex-crm
